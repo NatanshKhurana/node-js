2329a939d57349f7f26f91427799ed894768b7ec
Natansh14687 <natansh522@gmail.com>
2026-07-05 13:06:44 +0530

    concept of multiple route handlers and next() function

--- src/app.js
+++ src/app.js
@@ -2,32 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.use("/route", (req, res, next) => {
+  console.log("This is first route handler !!");
+  res.send("Response 1");
+  next();
+}, (req, res, next) => {
+  console.log("This is the Second route handler !!");
+  // res.send("Response 2");
+  next();
+}, (req, res, next) => {
+  console.log("This is the third route handler !!");
+  res.send("Response 3");
+})
 
 
-// This will only handle GET calls to /user
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ name: "Natansh", age: 22 });
-});
-
-app.post("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send("User added successfully");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User deleted !!");
-});
-
-// This route will dominate over all the http methods GET, POST, DELETE etc because is applicable for all methods and dominant so should be placed belo them or not placed if using HTTP methods
-app.use("/user", (req, res) => {
-  res.send("Hahahahahaha  !!!!");
-});
-
-// This will match all the HTTP method api calls to /test
-app.use("/test", (req, res) => {
-  res.send("This is test route");
-});
 
 app.listen(7777, () => {
   console.log("App is successfully listening at port 7777");
