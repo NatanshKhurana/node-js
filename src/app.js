50a64e7646dce2014beca14fa77cb51236677313
Natansh14687 <natansh522@gmail.com>
2026-07-05 17:04:58 +0530

    post /signup api call to database through user instance of User model

--- src/app.js
+++ src/app.js
@@ -3,6 +3,25 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("../src/config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  // Creating an instance of the User model
+  const user = new User({
+    firstName: "Dushyant",
+    lastName: "Sharma",
+    age: 22,
+    email: "dushyant@sharma.com",
+    password: "hii@123",
+  });
+
+  try {
+    await user.save();
+    res.send("User signed up successfully");
+  } catch (err) {
+    res.status(500).send("Something went wrong while posting data to database");
+  }
+});
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://khurananatansh_db_user:ArNatansh14687@namastenode.3spyyvy.mongodb.net/DevTinder");
+    await mongoose.connect("mongodb+srv://khurananatansh_db_user:ArNatansh14687@namastenode.3spyyvy.mongodb.net/devTinder");
 }
 
 module.exports = connectDB;
