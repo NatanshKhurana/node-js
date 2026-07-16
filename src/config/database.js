7d104f3797662ce9c959dff94b9630a6cf5105bf
Natansh14687 <natansh522@gmail.com>
2026-07-16 12:49:56 +0530

    updated db password

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://khurananatansh_db_user:ArNatansh14687@namastenode.3spyyvy.mongodb.net/devTinder");
+    await mongoose.connect("mongodb+srv://khurananatansh_db_user:oQ4QeTbxsiI8PIp1@namastenode.3spyyvy.mongodb.net/devTinder");
 }
 
 module.exports = connectDB;
